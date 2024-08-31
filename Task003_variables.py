b0193649621fedbf57c4bb1da98194b50f10a2a5
skarnauhov <62534771+skarnauhov@users.noreply.github.com>
2024-08-31 12:14:06 +0300

    Add files via upload

---
+++ Task003_variables.py
@@ -0,0 +1,5 @@
+quantity_of_hometasks = 12
+quantity_of_hours = 1.5
+course_name = 'Python'
+time_for_one_task = quantity_of_hours / quantity_of_hometasks
+print('Курс: ' + course_name + ', всего задач: ' + str(quantity_of_hometasks) + ', затрачено часов: ' + str(quantity_of_hours) + ', среднее время выполнения',time_for_one_task,'часа.')
