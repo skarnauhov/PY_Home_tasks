2a7824e6a16c773e5ce1dbe843ac0fc196fa784b
skarnauhov <62534771+skarnauhov@users.noreply.github.com>
2024-08-31 11:18:18 +0300

    Add files via upload

---
+++ Task002_String_Index.py
@@ -0,0 +1,6 @@
+example = 'Практическое задание по уроку "Строки и индексация строк"'
+print(example[0])
+print(example[-1])
+print(example[(len(example) // 2)::])
+print(example[::-1])
+print(example[1::2])
