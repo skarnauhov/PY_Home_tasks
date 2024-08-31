eeebd34d60f0fac4ece63657b035b0cee58dc9a2
skarnauhov <62534771+skarnauhov@users.noreply.github.com>
2024-08-31 13:36:51 +0300

    Add files via upload

---
+++ Task004_dynamic_type.py
@@ -0,0 +1,12 @@
+from operator import truediv
+
+name = ('Sergey')
+print ('Name:',name)
+age = 41
+print('Age: ' + str(age))
+age = age + 1
+print('New age: ' + str(age))
+is_student = True
+print('Is student: ' + str(is_student))
+
+
