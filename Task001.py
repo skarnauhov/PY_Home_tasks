6f44bcdd097715acfd433f712e51c69a18a71a5a
skarnauhov <62534771+skarnauhov@users.noreply.github.com>
2024-09-01 09:35:43 +0300

    Add files via upload

---
+++ Task001.py
@@ -0,0 +1,18 @@
+# 1st program
+from traceback import print_exc
+
+print(9 ** 0.5 * 5)
+
+# 2nd program
+
+print(99.9 > 99.8 and 1000 != 1001)
+
+# 3rd program
+
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4th program
+
+print(int((float('123.456') * 10)) % 10)
