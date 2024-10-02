cb7ac88f58a05eadd18bfdcd58e82f96f414d191
skarnauhov <62534771+skarnauhov@users.noreply.github.com>
2024-10-02 22:55:49 +0300

    Add files via upload

---
+++ Module_5_3.py
@@ -0,0 +1,108 @@
+
+class House:
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            for i in range(new_floor - 1):
+                print(i + 1)
+            print(f'{new_floor} - Приехали!')
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __eq__(self, other):
+        if isinstance(self, House) and isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        else:
+            return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(self, House) and isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        else:
+            return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(self, House) and isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        else:
+            return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(self, House) and isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        else:
+            return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(self, House) and isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        else:
+            return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(self, House) and isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        else:
+            return NotImplemented
+
+    def __add__(self, other):
+        if isinstance(self, House) and isinstance(other, House):
+            self.number_of_floors = self.number_of_floors + other.number_of_floors
+            return self
+        elif isinstance(self, House) and isinstance(other, int):
+            self.number_of_floors = self.number_of_floors + other
+            return self
+        else:
+            return NotImplemented
+
+    def __radd__(self, other):
+        if isinstance(self, House) and isinstance(other, int):
+            self.number_of_floors = self.number_of_floors + other
+            return self
+        else:
+            NotImplemented
+
+    def __iadd__(self, other):
+        if isinstance(self, House) and isinstance(other, House):
+            self.number_of_floors = self.number_of_floors + other.number_of_floors
+            return self
+        elif isinstance(self, House) and isinstance(other, int):
+            self.number_of_floors = self.number_of_floors + other
+            return self
+        else:
+            return NotImplemented
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
