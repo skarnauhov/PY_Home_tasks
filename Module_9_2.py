4cfe65ae7702c71ef9ae927efb114ad30ecc6bff
skarnauhov <62534771+skarnauhov@users.noreply.github.com>
2024-10-31 20:37:00 +0300

    Add files via upload

---
+++ Module_9_2.py
@@ -0,0 +1,13 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result =[len(x) for x in first_strings if len(x) > 5]
+print(first_result)
+print()
+
+second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
+print(second_result)
+print()
+
+third_result = [{x: len(x)} for x in first_strings + second_strings]
+print(third_result)
